aa8e7dfa2c9e01193502281541e735a30bcbc5cb
LGD-P <nplgdjp@gmx.Com>
2023-09-18 15:52:23 +0200

    [X] points are updated

--- tests/unit_tests/test_no_redeem_points_more_than_available.py
+++ tests/unit_tests/test_no_redeem_points_more_than_available.py
@@ -17,3 +17,16 @@ def test_not_enought_points_to_purchase(client, mocker_loadClubs, mocker_loadCom
         print(error_messages)
 
         assert error_messages.contents[0].strip() == "Choice not available please check your points"
+
+
+
+
+def test_club_points_are_updated(client,mocker_loadClubs,mocker_loadCompetitions):
+    clubs = mocker_loadClubs
+    club = [club for club in clubs if club['name'] == 'Dead Lift'][0]
+    initial_points = club["points"]
+    assert initial_points == "11"
+
+    client.post('/purchasePlaces', data={"club": "Dead Lift","competition": "Summer Festival","places": 2})
+    updated_points = club["points"]
+    assert updated_points == "9"
