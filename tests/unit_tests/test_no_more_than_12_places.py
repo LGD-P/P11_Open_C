811bec8c852207c95308cf67f131e3a563206104
LGD-P <nplgdjp@gmx.Com>
2023-09-19 14:09:56 +0200

    [X] test 12 points max to purchase

--- tests/unit_tests/test_no_more_than_12_places.py
+++ tests/unit_tests/test_no_more_than_12_places.py
@@ -1,6 +1,16 @@
+from bs4 import BeautifulSoup
 
 
-def test_enought_points_to_purchase(client,mocker_loadClubs,mocker_loadCompetitions):
-    valid_response = client.post('/purchasePlaces', data={"club": "Dead Lift","competition": "Summer Festival",
-                                                          "places": '2'})
-    assert valid_response.status_code == 200
+def test_less_or_equal_12_places_to_purchase(client,mocker_loadClubs,mocker_loadCompetitions):
+    valid_max_purchase = 12
+
+    expected_message = "Great-booking complete!".strip()
+
+    response = client.post('/purchasePlaces', data={"club": "Strong girls",
+                                                    "competition": "Heavy Fest",
+                                                    "places": str(
+                                                        valid_max_purchase)})
+
+    soup = BeautifulSoup(response.data, 'html.parser')
+    message = soup.find('li', {'id': 'error_message'})
+    assert message.contents[0].strip() == expected_message
