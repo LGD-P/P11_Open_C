87e28f0f0fb7923c3de11b4ee60f25a71729ee22
LGD-P <nplgdjp@gmx.Com>
2023-09-20 17:28:01 +0200

    [X] test data in clubs-list.html == data in .json

--- tests/unit_tests/test_clubs_list.py
+++ tests/unit_tests/test_clubs_list.py
@@ -1,2 +1,18 @@
+from bs4 import BeautifulSoup
+
+
 def test_is_clubs_list_reachable(client, mocker_loadClubs):
     assert client.get('/clubs-list')
+
+
+def test_is_clubs_list_displayed(client, mocker_loadClubs):
+    response = client.get('/clubs-list')
+    soup = BeautifulSoup(response.data, "html.parser")
+    rows = soup.find_all('tr', class_='data')
+    datas =  sorted(mocker_loadClubs, key=lambda x: int(x['points']), reverse=True)
+    assert len(rows)  == len(mocker_loadClubs)
+    for i, row in enumerate(rows):
+        cells = row.find_all('td')
+        assert cells[0].text.strip() == datas[i]['points']
+        assert cells[1].text.strip() == datas[i]['name']
+        assert cells[2].text.strip() == datas[i]['email']
